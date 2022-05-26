491ea7a0208163cd032842f3a7027c88ed3f7255
Rushanksavant <rushanksavant34@gmail.com>
2022-05-27 01:30:14 +0530

    getting started

---
+++ 1.Hello World/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\rssav\Desktop\Rust\1.Hello World\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\rssav\Desktop\Rust\1.Hello World\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ 1.Hello World/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\rssav\Desktop\Rust\1.Hello\ World\hello_cargo\target\debug\hello_cargo.exe: C:\Users\rssav\Desktop\Rust\1.Hello\ World\hello_cargo\src\main.rs
